978af4d5e5909ad6fcf6ffadf0c1e149d5f52b9e
Nathan Lane <lanenathan214@gmail.com>
2024-02-26 13:46:16 -0500

    started a generate password function

--- Develop/script.js
+++ Develop/script.js
@@ -10,8 +10,12 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+ function generatePassword(){
 
-}
+ }
+  }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
