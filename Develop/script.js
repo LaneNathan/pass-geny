781999020160c1fe9cc5c53aaa2523e372c25c7e
Nathan Lane <lanenathan214@gmail.com>
2024-02-26 14:01:22 -0500

    set character set to be used in password genration

--- Develop/script.js
+++ Develop/script.js
@@ -3,14 +3,16 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
+var chars ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%*"
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
  function generatePassword(){
+  
 
  }
   }
