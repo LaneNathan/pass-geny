a8431008a834856d9d30e282f3167f868781dc3b
Nathan Lane <lanenathan214@gmail.com>
2024-02-28 17:07:31 -0500

    changed upper casing variable name

--- Develop/script.js
+++ Develop/script.js
@@ -26,15 +26,16 @@ var chars='';
 var length= prompt('Please enter a character length from 1-128!');
 
 if(length>8&&length<128){
-var casing =confirm('Would you like upper case letters?');
+var casingUp =confirm('Would you like upper case letters?');
 var numbersConfirm = confirm('Would you like it to contain numbers?');
 
-if(casing){
+if(casingUp){
   chars+=upperChars;
 }
 if(numbersConfirm){
   chars+=numbers;
 }
+
 for(var i =0; i<= length; i++){ 
     var randomIndex = Math.floor(Math.random() * chars.length);
     password += chars.charAt(randomIndex);
